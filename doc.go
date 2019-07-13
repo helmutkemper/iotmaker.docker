df91ef06b0b97ec2d0845e29726a6f8bee463811
Helmut Kemper <helmut.kemper@gamil.com>
2019-07-13 17:19:52 -0300

    initial commit

--- doc.go
+++ doc.go
@@ -1,6 +1,8 @@
-// Helmut Kemper 07/2019
-// helmut.kemper@gmail.com
-//
-// pt-br: este pacote tem por finalidade gerenciar containers
-// en: this is a package for managing containers
+/*
+ Helmut Kemper 07/2019
+ helmut.kemper@gmail.com
+
+ pt-br: este pacote tem por finalidade gerenciar containers
+ en: this is a package for managing containers
+*/
 package iotmaker_docker
