c2503c6df076b9096db731a8ea8bd477a009754d
Helmut Kemper <helmut.kemper@gmail.com>
2020-06-13 13:02:24 -0300

    initial commit

--- containerCreate.go
+++ containerCreate.go
@@ -9,24 +9,16 @@ import (
 )
 
 //
-// en: creates a container and automatically exposes the ports listed in the image
+// English: Creates a container and automatically exposes the ports listed in the image
 //   imageName: image name for download and pull
 //   containerName: unique container name
 //   RestartPolicy:
-//      KRestartPolicyNo - Do not automatically restart the container. (the
-//          default)
-//      KRestartPolicyOnFailure - Restart the container if it exits due to an
-//          error, which manifests as a non-zero exit code.
-//      KRestartPolicyAlways - Always restart the container if it stops. If it is
-//          manually stopped, it is restarted only when Docker daemon restarts or
-//          the container itself is manually restarted. (See the second bullet
-//          listed in restart policy details)
-//      KRestartPolicyUnlessStopped - Similar to always, except that when the
-//          container is stopped (manually or otherwise), it is not restarted
-//          even after Docker daemon restarts.
+//      KRestartPolicyNo - Do not automatically restart the container. (the default)
+//      KRestartPolicyOnFailure - Restart the container if it exits due to an error, which manifests as a non-zero exit code.
+//      KRestartPolicyAlways - Always restart the container if it stops. If it is manually stopped, it is restarted only when Docker daemon restarts or the container itself is manually restarted. (See the second bullet listed in restart policy details)
+//      KRestartPolicyUnlessStopped - Similar to always, except that when the container is stopped (manually or otherwise), it is not restarted even after Docker daemon restarts.
 //   mountVolumes: please use a factoryWhaleAquarium.NewVolumeMount()
-//      for a complete list of volumes exposed by image, use
-//      ImageListExposedVolumes(id) and ImageListExposedVolumesByName(name)
+//      for a complete list of volumes exposed by image, use ImageListExposedVolumes(id) and ImageListExposedVolumesByName(name)
 //
 //     Example:
 //       err, dockerSys := factoryDocker.NewClient()
