99457345b0b9e0740462e6eb86ad975bd216fe01
Helmut Kemper <helmut.kemper@gmail.com>
2020-06-15 15:20:07 -0300

    working

--- containerFindIdByName.go
+++ containerFindIdByName.go
@@ -3,6 +3,7 @@ package iotmakerDocker
 import (
 	"errors"
 	"github.com/docker/docker/api/types"
+	"strings"
 )
 
 func (el *DockerSystem) ContainerFindIdByName(name string) (err error, id string) {
@@ -12,6 +13,24 @@ func (el *DockerSystem) ContainerFindIdByName(name string) (err error, id string
 	for _, containerData := range list {
 		for _, containerName := range containerData.Names {
 			if containerName == name {
+				id = containerData.ID
+				return
+			}
+		}
+	}
+
+	err = errors.New("container name not found")
+
+	return
+}
+
+func (el *DockerSystem) ContainerFindIdByNameContains(containsName string) (err error, id string) {
+	var list []types.Container
+
+	err, list = el.ContainerListAll()
+	for _, containerData := range list {
+		for _, containerName := range containerData.Names {
+			if strings.Contains(containerName, containsName) == true {
 				id = containerData.ID
 				return
 			}
