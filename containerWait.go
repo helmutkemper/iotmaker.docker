a8c230af1a2cc31895977592454ba289242bb081
Helmut Kemper <helmut.kemper@gmail.com>
2020-06-15 11:19:34 -0300

    working

--- containerWait.go
+++ containerWait.go
@@ -1,16 +1,13 @@
 package iotmakerDocker
 
-import (
-	"fmt"
-)
-
-//todo: fazer
-func (el *DockerSystem) ContainerWait(id string) {
+func (el *DockerSystem) ContainerWait(id string) (err error) {
 	wOk, wErr := el.cli.ContainerWait(el.ctx, id, "not-running")
 	select {
 	case <-wOk:
-		fmt.Println()
-	case <-wErr:
-		fmt.Println()
+
+	case err = <-wErr:
+
 	}
+
+	return
 }
