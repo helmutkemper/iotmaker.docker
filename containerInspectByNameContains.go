5ade24857938473757d2d3545a0584af4993dc54
Helmut Kemper <helmut.kemper@gmail.com>
2020-07-11 18:00:32 -0300

    working

---
+++ containerInspectByNameContains.go
@@ -0,0 +1,33 @@
+package iotmakerDocker
+
+import (
+	"github.com/docker/docker/api/types"
+)
+
+func (el *DockerSystem) containerInspectByNameContains(
+	name string,
+) (
+	err error,
+	list []types.ContainerJSON,
+) {
+
+	list = make([]types.ContainerJSON, 0)
+	var inspect types.ContainerJSON
+	var listOfContainers []NameAndId
+
+	err, listOfContainers = el.ContainerFindIdByNameContains(name)
+	if err != nil {
+		return
+	}
+
+	for _, v := range listOfContainers {
+		err, inspect = el.ContainerInspect(v.ID)
+		if err != nil {
+			return
+		}
+
+		list = append(list, inspect)
+	}
+
+	return
+}
