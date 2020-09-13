18ad8de44b5c871ba463840e190cdc3e2ea5b84e
Helmut Kemper <helmut.kemper@gmail.com>
2020-09-13 13:16:59 -0300

    documenting

--- v1.0.0.0-RC/doc.go
+++ v1.0.0.0-RC/doc.go
@@ -2,16 +2,7 @@
 // The idea behind this project is to facilitate the use of the moby project
 // https://github.com/moby/moby
 //
-// If all you want is to create and remove containers quickly and simply, please use the
-// whaleAquarium project
-// https://github.com/helmutkemper/iotmaker.docker.util.whaleAquarium
-//
-//
 // Package iotmakerdocker (Português): Projeto criado por Helmut Kemper
 // A ideia desse projeto é facilitar a utilização do projeto moby
 // https://github.com/moby/moby
-//
-// Se tudo o que você quer é subir e derrubar containers de forma rápida e simples, por
-// favor, user o projeto whaleAquarium
-// https://github.com/helmutkemper/iotmaker.docker.util.whaleAquarium
 package iotmakerdocker
