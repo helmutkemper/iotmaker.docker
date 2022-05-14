aed6cd2c6146e312fcaa45c42d4dc21e98cfa6af
Helmut Kemper <helmut.kemper@gmail.com>
2022-05-13 21:14:54 -0300

    new image build error message

--- v1.0.1/imagePull.go
+++ v1.0.1/imagePull.go
@@ -30,9 +30,15 @@ func (el *DockerSystem) ImagePull(
 	}
 
 	el.imageId[name] = ""
-	successfully := el.processBuildAndPullReaders(&reader, channel)
-	if successfully == false {
+	var successfully bool
+	successfully, err = el.processBuildAndPullReaders(&reader, channel)
+	if successfully == false || err != nil {
+		if err != nil {
+			return
+		}
+
 		err = errors.New("image pull error")
+		return
 	}
 
 	imageId, err = el.ImageFindIdByName(name)
